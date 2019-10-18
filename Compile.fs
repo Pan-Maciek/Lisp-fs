290876094dc2a2f3f62472ca2ed4f4bbf43a966e
koziejka <koziejka.com@gmail.com>
2019-10-18 10:32:50 +0200

    basic macro resolution

--- Compile.fs
+++ Compile.fs
@@ -1,7 +1,30 @@
 module Compile
 
 open Parse
+open Runtime
 
-let compile ast = 
+// GlobalContext.DefineMacro "module" 
+let moduleMacro (context: Context, ast: AST) =
+  printfn "Module Macro!"
+  match ast with
+  | List (Token name :: tail) -> 
+    context.DefineModule name ()
+    context, List tail
+  | _ -> failwith "Unexpected arguments to module macro"
 
-  ()
+let setupRuntime () =
+  GlobalContext.DefineMacro "module" moduleMacro
+  printfn "test"
+
+let rec resolveMacros (context: Context) (ast: AST) =
+  match ast with
+  | List (Token token::tail) -> 
+    match context.Macro token with
+    | None -> ast
+    | Some macro -> 
+      let context, ast = macro (context, List tail)
+      resolveMacros context ast
+  | List el -> List (el |> List.map (resolveMacros context))
+  | Token t -> Token t
+  | Empty -> Empty
+
--- Program.fs
+++ Program.fs
@@ -2,6 +2,8 @@
 
 open Lex
 open Parse
+open Compile
+open Runtime
 open System.IO
 
 [<EntryPoint>]
@@ -9,6 +11,10 @@ let main argv =
   let file = File.ReadAllText argv.[0]
   let tokens = tokenize file
   let ast = parse tokens
-  printfn "%A" tokens
-  printfn "%A" ast
+  setupRuntime ()
+  let macrosResolved = resolveMacros GlobalContext (List ast)
+
+  printfn "{%A}" ast
+  printfn "{%A}" macrosResolved
+  printfn "{%A}" GlobalContext
   0 // return an integer exit code
