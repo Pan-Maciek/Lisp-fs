8c4ab3f62d1812466f68c0bfa0ecdecfc484b961
koziejka <koziejka.com@gmail.com>
2019-10-18 13:23:24 +0200

    basic expression compilator

--- Compile.fs
+++ Compile.fs
@@ -15,13 +15,17 @@ let moduleMacro (context: Context, ast: AST) =
 let defnMacro (context: Context, ast: AST) =
   match ast with
   | List [Token name ; List args ; List body] ->
-    context.DefineFunction name (UserFunction (args, List body))
+    match context with
+    | :? Module as module' -> context.DefineFunction name (UserFunction (module', name, body))
+    | :? UserFunction as function' -> context.DefineFunction name (UserFunction (function', name, body))
+    | _ -> failwith "Function must be defined inside module or another user defined function"
     context, ast
-  | _ -> failwith "Unexpected arguments to module defn"
+  | _ -> failwith "Unexpected arguments to defn macro"
 
 let setupRuntime () =
   GlobalModule.DefineMacro "module" moduleMacro
   GlobalModule.DefineMacro "defn" defnMacro
+  GlobalModule.DefineFunction "print" (NativeFunction ("System.Console.Write"))
 
 let rec resolveMacros (context: Context) (ast: AST) =
   match ast with
@@ -35,3 +39,21 @@ let rec resolveMacros (context: Context) (ast: AST) =
   | Token t -> Token t
   | Empty -> Empty
 
+let (|FunctionCall|_|) (ast: AST, context: Context) =
+  match ast with
+  | List (Token name :: tail) -> 
+    match context.Function name with
+    | Some fn -> Some (fn, List.map (compileExpr context) tail)
+    | None -> None
+  | _ -> None
+
+let compileExpr (context: Context) (ast: AST) =
+  match ast, context with
+  | Token t, _ -> t
+  | FunctionCall (fn, args) -> 
+    match fn with
+    | :? UserFunction as fn -> fn.FullyQualifiedName + "(" + String.concat ", " args + ")"
+    | :? NativeFunction as fn -> fn.FullyQualifiedName + "(" + String.concat ", " args + ")"
+    | _ -> "" 
+  | List ops, _ -> String.concat " " (List.map (compileExpr context) ops)
+  | _ -> failwith ""
